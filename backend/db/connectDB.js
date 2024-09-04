63740e49e5a2534bd28096f783e15e1b589f077f
Muhammad Taha <mtaha.shakeel@systemsltd.com>
2024-09-04 07:15:20 -0700

    user Routes

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -1,16 +1,15 @@
 import mongoose from "mongoose";
 
-const connectDB = async()=>{
+const connectDB = async () => {
     try {
-        const conn =   await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log("MongoDB connected...");
+      const conn = await mongoose.connect(process.env.MONGO_URI);
+      console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
-        console.error("Error connecting to MongoDB:", error.message);
-        process.exit(1);
+      console.error("Error connecting to MongoDB:", error.message);
+      process.exit(1);
     }
-}
+  };
+  
+
 
 export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
 import dotenv  from "dotenv";
 import connectDB from './db/connectDB.js';
+import cookieParser from 'cookie-parser';
+import userRoutes from "./routes/userRoutes.js"
 
 dotenv.config();
 connectDB();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+//Middlewares (a function that runs between request and response)
+app.use(express.json()); //it allows u to pars(convert) incoming data from request into json formate
+app.use(express.urlencoded({ extended: true}));//express.urlencoded allows u to pars(convert) form data into req.body
+                                                //if true even if request body has some nested objects u can pars and vice versa
+app.use(cookieParser());//help to get the cookie from request and set the cookie inside response
 
+//Routes
+app.use("/api/users",userRoutes)
 app.listen(PORT,()=>console.log(`Server started at http://localhost:${PORT}`));
